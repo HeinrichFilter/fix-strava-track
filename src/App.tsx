ba7dffdf8f534deb457a9d049b0215a40d2d3e06
Heinrich Filter <heinrich@filteris.co.za>
2020-07-26 18:55:23 +0200

    Add upload field that can show XML text

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import { useDropzone } from "react-dropzone";
 
 function App() {
+  // const onDrop = useCallback((acceptedFiles) => {
+  //   // Do something with the files
+  //   console.log("dropped");
+  // }, []);
+  const { acceptedFiles, getRootProps, getInputProps, isDragActive } = useDropzone(/*{ onDrop }*/);
+  const [xml, setXml] = useState<string>();
+
+  const files = acceptedFiles.map((file) => (
+    <li key={file.name}>
+      {file.name} - {file.size} bytes
+    </li>
+  ));
+
+  useEffect(() => {
+    console.log("Accepted files changed");
+    async function getFileText() {
+      if (acceptedFiles.length < 1) return;
+      const firstAccpetedFile = acceptedFiles[0];
+      setXml(await firstAccpetedFile.text());
+    }
+    getFileText();
+  }, [acceptedFiles]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Fix Strava track</h1>
       </header>
+      <br />
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        {isDragActive ? <p>Drop the files here ...</p> : <p>Drag 'n' drop some files here, or click to select files</p>}
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+      <pre>{xml}</pre>
     </div>
   );
 }
